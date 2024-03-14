67ba37ec3a44e7337729ec9de022572f951f13fa
Hoxton <za546321@yahoo.com.tw>
2024-03-15 03:44:57 +0800

    Home Done

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <HomeView></HomeView>
 </template>
 
+<script setup>
+import HomeView from './views/HomeView.vue';
+
+</script>
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
